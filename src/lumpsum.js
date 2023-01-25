2d42e6eb53632ddbd68bbe44cc1e090cc2c095d1
ahmadswalih <ahmadswalih777@gmail.com>
2023-01-25 10:42:09 +0530

    added comments in the lumpsum equation section

--- src/lumpsum.js
+++ src/lumpsum.js
@@ -9,7 +9,7 @@ const LumpSumCalculator = () => {
   //Calculation function :- rate is constant here (ie is 25.2)
   // The equation used here is A=P*(1+(rate of Interest/100)^t) . where A= final Amount,P= Principal & t= time period
   const handleCalculate = () => {
-    setLumpSum((principal * Math.pow((1+(25.2/100)),time) ));
+    setLumpSum((principal * Math.pow((1+(25.2/100)),time) ));//rate of interest is constant here (ie is 25.2)
     console.log(lumpSum);
     
   };
